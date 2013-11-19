ec0d9a38044e299299b782c1953015b215c236f9
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2013-11-19 01:02:07 -0300

    Make ore-infinium use our build of QtQuick.

--- ext_oreinfinium.cmake
+++ ext_oreinfinium.cmake
@@ -7,10 +7,10 @@ ExternalProject_Add(
     GIT_REPOSITORY git://github.com/sreich/ore-infinium
     GIT_TAG qtquick-attempt
     CMAKE_CACHE_ARGS
-        -DCMAKE_PREFIX_PATH:STRING=${CMAKE_PREFIX_PATH};${CHIPMUNK_PREFIX};${YAMLCPP_PREFIX};${ENET_PREFIX}
+        -DCMAKE_PREFIX_PATH:STRING=${CMAKE_PREFIX_PATH};${CHIPMUNK_PREFIX};${YAMLCPP_PREFIX};${ENET_PREFIX};${QT_PREFIX}
         -DPROTOBUF_SRC_ROOT_FOLDER:PATH=${PROTOBUF_SRC}
         -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
         -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
 
-    DEPENDS Protobuf Chipmunk yaml-cpp enet
+    DEPENDS Protobuf Chipmunk yaml-cpp enet qtdeclarative
 )

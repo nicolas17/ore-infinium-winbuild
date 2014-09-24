2b964f7f1632c9e2de94abb2e6b59f4773e2ac96
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2014-09-24 00:56:11 -0300

    Switch to master branch of ore-infinium.

--- ext_oreinfinium.cmake
+++ ext_oreinfinium.cmake
@@ -5,7 +5,6 @@
 ExternalProject_Add(
     ore-infinium
     GIT_REPOSITORY git://github.com/sreich/ore-infinium
-    GIT_TAG qtquick-attempt
     CMAKE_CACHE_ARGS
         -DCMAKE_PREFIX_PATH:STRING=${CMAKE_PREFIX_PATH};${ENET_PREFIX};${QT_PREFIX}
         -DPROTOBUF_SRC_ROOT_FOLDER:PATH=${PROTOBUF_SRC}
